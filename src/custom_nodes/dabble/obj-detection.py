28531fe6a140147dcc70fb83d76143fec58b68e0
leezhongjun <leezhongjun4@gmail.com>
2023-01-14 02:22:45 +0800

    added filter detection for only one person/object in frame

--- src/custom_nodes/dabble/obj-detection.py
+++ src/custom_nodes/dabble/obj-detection.py
@@ -5,12 +5,22 @@ Node template for creating custom nodes.
 from typing import Any, Dict
 import threading
 from pathlib import Path
-import pygame
+import numpy as np
 
 import winsound
 
 from peekingduck.pipeline.nodes.abstract_node import AbstractNode
+from peekingduck.pipeline.nodes.draw.utils.bbox import draw_bboxes
 
+def filter_bbox(bbox_i, bbox_scores):
+    '''return bbox_i with max score'''
+    max_score = 0
+    max_i = -1
+    for i in bbox_i:
+        if bbox_scores[i] > max_score:
+            max_score = bbox_scores[i]
+            max_i = i
+    return max_i
 
 class Node(AbstractNode):
     """This is a template class of how to write a node for PeekingDuck.
@@ -18,10 +28,10 @@ class Node(AbstractNode):
     Args:
         config (:obj:`Dict[str, Any]` | :obj:`None`): Node configuration.
     """
-
     def __init__(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
         super().__init__(config, node_path=__name__, **kwargs)
         self.thread = None
+        self.show_labels = self._get_config_types()["show_labels"]  # type: bool
 
     def playsound(self, freq):
         if self.thread is None or not self.thread.is_alive():
@@ -38,21 +48,59 @@ class Node(AbstractNode):
             outputs (dict): Dictionary with keys "__".
         """
         bbox_labels = inputs["bbox_labels"]
-        target = [i for i in bbox_labels if i != "person"]
+        bbox_scores = inputs["bbox_scores"]
         obj_3D_locs = inputs["obj_3D_locs"]
+        bboxes = inputs["bboxes"]
 
-        print(f'bbox_labels: {bbox_labels}')
-        print(f'obj_3D_locs: {obj_3D_locs}')
-
-        if "person" in bbox_labels:
-            if len(target) == 0:
-                self.playsound(500)
+        items_i = []
+        persons_i = []
+        for i, x in enumerate(bbox_labels):
+            if x != "person":
+                items_i.append(i)
             else:
+                persons_i.append(i)
+        
+        person_i = filter_bbox(persons_i, bbox_scores)
+        item_i = filter_bbox(items_i, bbox_scores)
+
+        if person_i != -1:
+            if item_i == -1:
+                #only item on screen
+                self.playsound(500)
+
+            else:
+                #both on screen
                 freq = 3000
                 #####TODO: change this freq based on distance
+                
 
+                
                 self.playsound(freq)
-        elif len(target) > 0:
+
+        elif item_i != -1:
+            #only hand on screen
             self.playsound(300)
-        
+
+        #filter bboxes and labels
+        if person_i != -1:
+            if item_i != -1:
+                n_bboxes = [bboxes[person_i], bboxes[item_i]]
+                n_bbox_labels = [bbox_labels[person_i], bbox_labels[item_i]]
+            else:
+                n_bboxes = [bboxes[person_i]]
+                n_bbox_labels = [bbox_labels[person_i]]
+        elif item_i != -1:
+            n_bboxes = [bboxes[item_i]]
+            n_bbox_labels = [bbox_labels[item_i]]
+        else:
+            n_bboxes = []
+            n_bbox_labels = []
+        #draw bboxes
+        draw_bboxes(
+            inputs["img"], n_bboxes, n_bbox_labels, self.show_labels
+        )
         return {}
+
+    def _get_config_types(self) -> Dict[str, Any]:
+        """Returns dictionary mapping the node's config keys to respective types."""
+        return {"show_labels": bool}
