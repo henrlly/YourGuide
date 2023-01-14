494988aefbbee2777c3517fa11c758cec4bde042
leezhongjun <leezhongjun4@gmail.com>
2023-01-14 11:17:54 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 ### Dependencies
 playsound, gtts, pyaudio, peekingduck and SpeechRecognition, winsound
 
+NOTE: peekingduck is buggy with python 3.10 and above
+use python<=3.9.13
+
+get dependencies with
+```
+pip install -r requirements.txt
+```
+
 
 ### How to use
 Download folder and run run.py
--- src/custom_nodes/dabble/obj-detection.py
+++ src/custom_nodes/dabble/obj-detection.py
@@ -12,15 +12,6 @@ import winsound
 from peekingduck.pipeline.nodes.abstract_node import AbstractNode
 from peekingduck.pipeline.nodes.draw.utils.bbox import draw_bboxes
 
-def filter_bbox(bbox_i, bbox_scores):
-    '''return bbox_i with max score'''
-    max_score = 0
-    max_i = -1
-    for i in bbox_i:
-        if bbox_scores[i] > max_score:
-            max_score = bbox_scores[i]
-            max_i = i
-    return max_i
 
 class Node(AbstractNode):
     """This is a template class of how to write a node for PeekingDuck.
@@ -31,16 +22,14 @@ class Node(AbstractNode):
     def __init__(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
         super().__init__(config, node_path=__name__, **kwargs)
         self.thread = None
-        self.show_labels = self._get_config_types()["show_labels"]  # type: bool
 
-    def playsound(self, freq):
+    def playsound(self, freq, duration):
         if self.thread is None or not self.thread.is_alive():
-            self.thread = threading.Thread(target=winsound.Beep, args=(freq, 100), daemon=True)
+            self.thread = threading.Thread(target=winsound.Beep, args=(freq, duration), daemon=True)
             self.thread.start()
     
     def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:  # type: ignore
         """This node does ___.
-
         Args:
             inputs (dict): Dictionary with keys "__", "__".
 
@@ -52,34 +41,72 @@ class Node(AbstractNode):
         obj_3D_locs = inputs["obj_3D_locs"]
         bboxes = inputs["bboxes"]
 
-        items_i = []
-        persons_i = []
+        max_score_p = 0
+        max_score_item = 0
+        person_i = -1
+        item_i = -1
+
         for i, x in enumerate(bbox_labels):
-            if x != "person":
-                items_i.append(i)
+            if x == "person":
+                if bbox_scores[i] > max_score_p:
+                    max_score_p = bbox_scores[i]
+                    person_i = i
             else:
-                persons_i.append(i)
+                if bbox_scores[i] > max_score_item:
+                    max_score_item = bbox_scores[i]
+                    item_i = i
         
-        person_i = filter_bbox(persons_i, bbox_scores)
-        item_i = filter_bbox(items_i, bbox_scores)
 
         if person_i != -1:
             if item_i == -1:
                 #only item on screen
-                self.playsound(500)
+                self.playsound(500, 100)
 
             else:
                 #both on screen
-                freq = 3000
+                freq = 500
+                duration = 100
                 #####TODO: change this freq based on distance
-                
 
+                #3d distance
+                a = obj_3D_locs[person_i]
+                b = obj_3D_locs[item_i]
+                dist3d = np.linalg.norm(a-b)
+                print(dist3d)
+
+                #2d distance
+                px = (bboxes[person_i][0], bboxes[person_i][2])
+                py = (bboxes[person_i][1], bboxes[person_i][3])
+                ix = (bboxes[item_i][0], bboxes[item_i][2])
+                iy = (bboxes[item_i][1], bboxes[item_i][3])
+                a = np.array((bboxes[person_i][2] - bboxes[person_i][0], bboxes[person_i][3] - bboxes[person_i][1]))
+                b = np.array((bboxes[item_i][2] - bboxes[item_i][0], bboxes[item_i][3] - bboxes[item_i][1]))
+                print(a, b)
+                dist2d = np.linalg.norm(a-b)
                 
-                self.playsound(freq)
+                #min 2d distance
+                if px[0] >= ix[0] and px[0] <= ix[1]:
+                    #within x range
+                    if py[0] >= iy[0] and py[0] <= iy[1]:
+                        #within y range
+                        dist2d = 0
+                    else:
+                        dist2d = min(abs(py[0] - iy[0]), abs(py[0] - iy[1]))
+                else:
+                    if py[0] >= iy[0] and py[0] <= iy[1]:
+                        #within y range
+                        dist2d = min(abs(px[0] - ix[0]), abs(px[0] - ix[1]))
+                    else:
+                        dist2d = min(np.linalg.norm(np.array((px[0], py[0])) - np.array((ix[0], iy[0]))), np.linalg.norm(np.array((px[0], py[0])) - np.array((ix[1], iy[1]))))
+                
+                print(dist2d)
+
+
+                self.playsound(int(3000 - 2000*dist2d), duration)
 
         elif item_i != -1:
             #only hand on screen
-            self.playsound(300)
+            self.playsound(300, 100)
 
         #filter bboxes and labels
         if person_i != -1:
@@ -95,12 +122,9 @@ class Node(AbstractNode):
         else:
             n_bboxes = []
             n_bbox_labels = []
+
         #draw bboxes
         draw_bboxes(
-            inputs["img"], n_bboxes, n_bbox_labels, self.show_labels
+            inputs["img"], n_bboxes, n_bbox_labels, True
         )
         return {}
-
-    def _get_config_types(self) -> Dict[str, Any]:
-        """Returns dictionary mapping the node's config keys to respective types."""
-        return {"show_labels": bool}
