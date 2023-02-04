8a663e8fbb3c38641aff46c6de9b22fc4914129a
TheRealGod <52273591+GitHub-OfficialAccount@users.noreply.github.com>
2023-02-04 20:14:52 +0800

    Clean

--- src/scripts/tts_tool.py
+++ src/scripts/tts_tool.py
@@ -2,13 +2,6 @@ import winsound
 from gtts import gTTS
 from pydub import AudioSegment
 
-# #proxy setting (if required)#
-# try:
-#     from misc.proxy_config import change_proxy
-# except:
-#     from src.misc.proxy_config import change_proxy
-# change_proxy('http://127.0.0.1:10809')
-
 def tts(text):
     tts_t = gTTS(text)
     tts_t.save("sounds/audio.mp3")
